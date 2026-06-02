d107bc3f36b910e291d69905944a9f144ff0cc5c
Aman <amanadv2022@gmail.com>
2026-06-02 13:39:59 +0530

    Updated code

--- function.js
+++ function.js
@@ -13,13 +13,48 @@
 
 
 //-----------arrow function---------------
-//  const sum=(a,b)=>{
-//     console.log(a+b);
-//  }
-//  sum(1,3);
+//  ---------------foreachloop in array---------
+// let arr=[1,2,3,4,5];
+// arr.forEach((val)=>{
+//     console.log(val);
+// })
 
-//  const mul=(a,b)=>{
-//    // console.log(a*b); 
-//     return a*b; 
-//  }
-//  console.log(mul(4,3));
+//------optionalparametres foreachloop----
+// let arr=[1,2,3,4,5];
+// arr.forEach((val,idx,arr)=>{
+//     console.log(val,idx,arr);
+// })
+
+
+
+// //-----------------map method-----------------
+// let arr=[1,2,3,4,5];
+// let arrNem=arr.map((val)=>{
+//     return val*2;
+// })
+// console.log(arrNem);
+
+
+
+
+//--------------------filter method----------
+//  let arr=[1,2,3,4,5];
+//  let newArr=arr.filter((val)=>{
+//     return val%2!==0;
+//  })
+//  console.log(newArr);
+
+
+//--------------------reduce method--------------
+// let arr=[1,2,3,4,5];
+// let output=arr.reduce((res,cur)=>{
+//     return res>cur;
+// });
+// console.log(output);
+
+
+// let arr=[1,2,7,3,4,5];
+// let output=arr.reduce((res,cur)=>{
+//     return res > cur ? res:cur;
+// });
+// console.log(output);
--- practicefunction.js
+++ practicefunction.js
@@ -21,17 +21,65 @@
 
 //---------------merhod 2-----------
 
-function countVowels(str){
-   //n=str.length;
-   let count=0;
- for(let char of str){
-    if(char==="a"|char==="e"|| char==="i"||char==="o"||char==="u"){
-        count++;
-        console.log(char);
-    }
+// function countVowels(str){
+//    //n=str.length;
+//    let count=0;
+//  for(let char of str){
+//     if(char==="a"|char==="e"|| char==="i"||char==="o"||char==="u"){
+//         count++;
+//         console.log(char);
+//     }
 
     
- }
-   console.log(count);
-};
-countVowels("apnacollege");
+//  }
+//    console.log(count);
+// };
+// countVowels("apnacollege");
+
+
+
+//------------------question 2 for each loop-------
+//  let arr=[1,2,3,4,5];
+//  arr.forEach((val)=>{
+//     let s=val*val;
+//     console.log(s);
+//  })
+
+
+
+//-------------------question 3 filter out------
+// let arr=[87,23,90,54,93,94,95,96,97];
+// let output=arr.filter((val)=>{
+//     return val>90;
+// })
+// console.log(output);
+
+
+//----------------ques 4 reduce method-------
+let n=prompt("enter a number");
+let arr=[];
+for(let i=0;i<n;i++){
+    arr[i]=i+1;
+}
+console.log(arr);
+let output=arr.reduce((res,cur)=>{
+    return res+cur;
+})
+console.log(output);
+let prooutput=arr.reduce((res,cur)=>{
+    return res*cur;
+})
+console.log(prooutput);
+
+
+
+
+
+// //------method 2-----------
+// let n=prompt("enter a number");
+// let arr=n.split(",").map(Number);
+// console.log(arr);
+// let output=arr.reduce((res,cur)=>{
+//     return res+cur;
+// })
+// console.log(output);
