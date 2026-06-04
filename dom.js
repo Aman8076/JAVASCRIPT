4d6c0823203e7bde2c9c381f209221179739ec86
Aman <amanadv2022@gmail.com>
2026-06-04 10:34:12 +0530

    Updated code

--- dom.js
+++ dom.js
@@ -16,5 +16,72 @@
 // console.dir(headings); 
 // console.log(headings)
 
-let para=document.getElementsByTagName("p");
-console.dir(para);
+// let para=document.getElementsByTagName("p");
+// console.dir(para);
+
+
+
+//----------query selector----------
+// let q=document.querySelector("p")
+// console.dir(q);
+
+// let q=document.querySelectorAll("p")
+// console.dir(q);
+
+// let q=document.querySelectorAll(".heading-class")
+// console.dir(q);
+
+
+
+
+//-----------------properties-----------------
+// let q=document.querySelector("p")
+// console.dir(q);
+// console.log(q.tagName);
+
+// let div=document.querySelector("div");
+// console.dir(div);
+// console.log(div.innerText);
+// console.log(div.innerHTML)
+
+// div.innerText="abcd";
+// let heading=document.querySelector("h1");
+//console.dir("heading");
+// heading.innerHTML="<b>aman</b>"
+// console.log(heading.innerText);
+
+
+
+
+
+
+
+//----------------------question 1-----------------
+
+//===============method 1=============
+// let h2=document.querySelector("h2");
+// console.dir(h2.innerText);
+//  h2.innerText=h2.innerText+"from apna college students";
+
+
+// //=====method 2==============
+//  let k=h2.innerText;
+//  let m="from apna colleges students";
+// h2.innerText=k.concat(m);
+
+
+
+//------------------------question 2----------------
+// let divs=document.querySelectorAll(".box");
+// console.dir(divs);
+// divs[0].innerText="new value 1";
+// divs[1].innerText="new value 2";
+// divs[2].innerText="new value 3";
+
+//==================method 2===========
+let divs=document.querySelectorAll(".box");
+let idx=1;
+for (k of divs){
+    k.innerText=`new unique value ${idx}`;
+    idx++;
+}
